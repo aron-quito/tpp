5faae3d1a36807945c4332fc99c07a48bcc9f529
jamamanica <jamamanica@unjbg.edu.pe>
2026-07-13 08:47:30 -0500

    Define que informacion se guardara de cada variable

---
+++ tabla_simbolos.h
@@ -0,0 +1,27 @@
+#ifndef TABLA_SIMBOLOS_H
+#define TABLA_SIMBOLOS_H
+
+/* Tipos de datos que soporta nuestro lenguaje */
+typedef enum {
+    T_ENTERO,
+    T_DECIMAL,
+    T_CADENA,
+    T_FUNCION
+} TipoDato;
+
+/* La estructura para cada símbolo (variable o función) */
+typedef struct Simbolo {
+    char *nombre;          // Nombre de la variable/función (ej. "promedio")
+    TipoDato tipo;         // Tipo de dato (T_ENTERO, T_DECIMAL, etc.)
+    int es_funcion;        // 1 si es función, 0 si es variable
+    int alcance;           // Ámbito/Scope (0 para global, 1, 2... para locales)
+    struct Simbolo *sig;   // Puntero al siguiente símbolo (Lista enlazada)
+} Simbolo;
+
+/* Prototipos de las funciones de la tabla */
+void insertar_simbolo(char *nombre, TipoDato tipo, int es_funcion, int alcance);
+Simbolo* buscar_simbolo(char *nombre);
+void imprimir_tabla();
+void liberar_tabla();
+
+#endif
